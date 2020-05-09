feea82411b1b8fe648f0b2158356b6c1122d07a7
bernardkkt <kianshunkin@gmail.com>
2020-05-09 13:22:54 +0800

    WIP

---
+++ src/decorator.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+class Hashes:
+    def __init__(self):
+        return
+    
+    def print_ln(self):
+        return
+    
+    @print_ln
+    def base64(self):
+        return
+
+def main():
+    return
+
+if __name__ == "__main__":
+    main()
